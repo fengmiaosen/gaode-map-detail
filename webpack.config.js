99bfae2385b7014127fd995f6e09ff594664f410
fengmiaosen <fengmiaosen@gmail.com>
2016-12-25 16:13:13 +0800

    基于Vue2+wepback2

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,26 @@ module.exports = {
             vue: 'vue/dist/vue.js' //webpack打包时，需要设置别名
         }
     },
-    plugins: [
-        // new webpack.optimize.UglifyJsPlugin()
-    ]
+    plugins: []
 };
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.devtool = '#source-map'
+    // http://vue-loader.vuejs.org/en/workflow/production.html
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        })
+    ])
+}
